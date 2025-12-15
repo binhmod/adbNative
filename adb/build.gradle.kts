86086f523c531d2e70f558bf67c5e6728e537252
binhmod <thaibinhphantang@gmail.com>
2025-12-15 21:23:46 +0700

    Initial commit

---
+++ adb/build.gradle.kts
@@ -0,0 +1,73 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "me.binhmod.adb"
+    compileSdk {
+        version = release(36)
+    }
+
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+
+        externalNativeBuild {
+            cmake {
+                arguments += "-DANDROID_STL=none"
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = project.file("src/main/cpp/CMakeLists.txt")
+            version = "3.31.0"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        prefab = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildToolsVersion = "36.0.0"
+    ndkVersion = "29.0.14206865"
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.bcpkix.jdk18on)
+    implementation(libs.androidx.annotation.jvm)
+    implementation(libs.androidx.lifecycle.livedata.core.ktx)
+
+    implementation(libs.boringssl)
+    implementation(libs.libcxx)
+
+    implementation(libs.rikka.hidden.compat)
+    compileOnly(libs.rikka.hidden.stub)
+}
